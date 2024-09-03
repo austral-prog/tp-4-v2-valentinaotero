7b35808b3bfadd4c531409f86c4fcb02cba84503
Juan Longo <juan.longo@mulesoft.com>
2024-09-03 17:34:28 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,10 +1,14 @@
-def es_bisiesto(leap_year):
-    return (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0)
+def leap_year():
+    leap_year = int(input("Ingrese un año: "))
 
-leap_year = int(input("Ingrese un año: "))
-
-if es_bisiesto(leap_year):
-    print(f"El año {leap_year} es bisiesto")
-else:
-    print(f"El año {leap_year} no es bisiesto")
+    is_leap_year = (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0)
+    return f"El año {leap_year} {"es" if is_leap_year else "no es"} bisiesto"
+    
+    is_leap_year = (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0)
+    return f"El año {leap_year} es bisiesto" if is_leap_year else f"El año {leap_year} no es bisiesto"
+    
+    if (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0):
+        return f"El año {leap_year} es bisiesto"
+    else:
+        return f"El año {leap_year} no es bisiesto"
 
