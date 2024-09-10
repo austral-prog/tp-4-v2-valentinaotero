55782fa9fa9a5db0841285ceddb4d077c2986f5c
valentinaotero <162721713+valentinaotero@users.noreply.github.com>
2024-09-09 21:07:36 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -3,12 +3,3 @@ def leap_year():
 
     is_leap_year = (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0)
     return f"El año {leap_year} {"es" if is_leap_year else "no es"} bisiesto"
-    
-    is_leap_year = (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0)
-    return f"El año {leap_year} es bisiesto" if is_leap_year else f"El año {leap_year} no es bisiesto"
-    
-    if (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0):
-        return f"El año {leap_year} es bisiesto"
-    else:
-        return f"El año {leap_year} no es bisiesto"
-
